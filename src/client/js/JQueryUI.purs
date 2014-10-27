b7748fdefbdbdad1514ca0142b1ce8d8b7158ae7
TheBizzle <jason.bertsche@gmail.com>
2014-10-26 23:30:28 -0500

    Unintroducing `selectThis`

--- src/client/js/JQueryUI.purs
+++ src/client/js/JQueryUI.purs
@@ -133,14 +133,6 @@ foreign import val
   }
   """ :: forall eff. String -> JQuery -> Eff (dom :: DOM | eff) JQuery
 
--- $(this)
-foreign import selectThis
-  """
-  function selectThis() {
-    return jQuery(this);
-  }
-  """ :: forall eff. Eff (dom :: DOM | eff) JQuery
-
 -- .attr(str)
 foreign import attrStr
   """
