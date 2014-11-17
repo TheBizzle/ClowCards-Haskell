a8af8b6da6222219d392fafd9d77e0cc1d724642
TheBizzle <jason.bertsche@gmail.com>
2014-11-17 00:17:22 -0600

    Implementing more of the onload page-processing behavior
    Most notably here, the collection of "card is enabled" toggleboxes is
    now being loaded onto the page.  Pretty spiffy!

--- src/client/js/Main.purs
+++ src/client/js/Main.purs
@@ -1,13 +1,21 @@
 module Main (main) where
 
 import Control.Monad.Eff(Eff(..))
-import Control.Monad.JQuery(JQuery(..), ready, select)
+import Control.Monad.JQuery(append, css, JQuery(..), ready, select)
 
+import Data.Foldable(for_)
+
+import AppState(AppState(..))
+import Cards(Card(..), cards)
 import DOM(DOM(..))
-import JQueryUI(accordion, attrStr, button, buttonset, change, click, each, flipChecked, refreshButton, siblings, spinner, tabs, val)
+import Element(genCardCheckbox)
+import JQueryUI(accordion, attrStr, button, buttonset, change, children, click, each, flipChecked, refreshButton, siblings, spinner, tabs, val)
 
 main = ready $ do
   initJQUI
+  AppState { cardHolder = cardHolder } <- initState
+  cleanupCSS
+  populateCardSetCustomizer cardHolder
 
 initJQUI :: forall eff. Eff (dom :: DOM | eff) JQuery
 initJQUI = do
@@ -33,9 +41,48 @@ initJQUI = do
       spinIt this = do
                       spinner this
                       val "1" this
-      innerClick this = do
-                          forAttr <- attrStr "for" this
-                          btn     <- select $ "#" ++ forAttr
-                          flipChecked btn
-                          refreshButton btn
-                          change btn
+
+cleanupCSS :: forall eff. Eff (dom :: DOM | eff) JQuery
+cleanupCSS = do
+  mainBox <- byID "main-box"
+  css { overflow: "auto" } mainBox
+
+initState :: forall eff. Eff (dom :: DOM | eff) AppState
+initState = do
+  adderButtonElem    <- byID "adder-button"
+  adderTableElem     <- byID "adder-table"
+  cardHolderElem     <- byID "card-holder"
+  cardNumSpinnerElem <- byID "card-num-spinner"
+  nameInputElem      <- byID "name-input"
+  pickButtonElem     <- byID "pick-btn"
+  return $ AppState { adderButton:    adderButtonElem
+                    , adderTable:     adderTableElem
+                    , cardHolder:     cardHolderElem
+                    , cardNumSpinner: cardNumSpinnerElem
+                    , nameInput:      nameInputElem
+                    , pickButton:     pickButtonElem
+                    , playerNums:     []
+                    }
+
+populateCardSetCustomizer :: forall eff. JQuery -> Eff (dom :: DOM | eff) JQuery
+populateCardSetCustomizer cardHolder = do
+  for_ cards $ appendCard cardHolder
+  foreach ".dynamic-check-button" button
+  foreach ".dynamic-check-label"  innerClick
+    where
+      foreach x f = children x cardHolder >>= f
+      appendCard elem (Card { name = name, isEnabled = isEnabled }) =
+        genCardCheckbox name isEnabled >>= flip append elem
+
+innerClick :: forall eff. JQuery -> Eff (dom :: DOM | eff) JQuery
+innerClick this = do
+  forAttr <- attrStr "for" this
+  btn     <- byID forAttr
+  flipChecked btn
+  refreshButton btn
+  change btn
+
+byID :: forall eff. String -> Eff (dom :: DOM | eff) JQuery
+byID id = select $ "#" ++ id
+
+(|>) a f = f a
